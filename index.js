220dd980810f6609f82597b25d59df3d0689341f
Oscar Estrada <arturostrada2020@gmail.com>
2022-08-20 21:39:17 -0500

    connect mongoose

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const morgan = require('morgan')
 const helmet = require('helmet')
 const genres = require('./routes/genres')
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
 
 require('dotenv').config()
 const PORT = process.env.PORT || 5000
+
+mongoose.connect(process.env.MONGODB_URL)
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connected to MongoDB', err))
 
 // Middlewares
 app.use(express.json())
