f02e0c81c8bdb3e55835aebff9edd16817e865ae
Oscar Estrada <arturostrada2020@gmail.com>
2022-08-31 23:04:10 -0500

    add return customer in delete endpoint

--- routes/customers.js
+++ routes/customers.js
@@ -33,7 +33,7 @@ router.put('/:id', async (req, res) => {
   const { error } = validateCustomer(req.body)
   if (error) return res.status(400).send(error.details[0].message)
 
-  const customer = await CustomerModel().findByIdAndUpdate(
+  const customer = await CustomerModel.findByIdAndUpdate(
     req.params.id,
     {
       isGold: req.body.isGold,
@@ -47,10 +47,13 @@ router.put('/:id', async (req, res) => {
 })
 
 router.delete('/:id', async (req, res) => {
-  const customer = await CustomerModel().findByIdAndRemove(req.params.id)
+  const customer = await CustomerModel.findByIdAndRemove(req.params.id)
   if (!customer) return res.status(404).send('The customer was not found')
 
-  return res.send(`The customer with the ID: ${customer.id} was deleted successfully`)
+  return res.send({
+    customer,
+    message: `The customer with the ID: ${customer.id} was deleted successfully`
+  })
 })
 
 module.exports = router
